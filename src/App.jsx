4d765bf76e5cea8e0cffee0ab076eadd381486d2
Josh B <joshua.banaga@yahoo.com>
2023-11-28 13:13:18 -0800

    reverted button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { memo, useState } from "react";
+import { useState } from "react";
 import "../public/styles/App.css";
 import Header from "./Header";
 import Screen from "./Screen";
@@ -8,66 +8,10 @@ function App() {
 
   //state for the currently displayed key
   const [displayVal, setDisplayVal] = useState("");
-  const [memory, setMemory] = useState(0);
 
   //The callback function that will be passed to every button component.
   const onBtnClick = ({ type, text, value }) => {
     switch (type) {
-      case "number":
-        if (text === "." && displayVal.includes(".")) {
-          break;
-        }
-        setDisplayVal((prevDisplayVal) => prevDisplayVal + text);
-        break;
-      case "operator":
-        setDisplayVal((prevDisplayVal) => prevDisplayVal + text);
-        break;
-      case "equal":
-        try {
-          setDisplayVal(eval(displayVal).toString());
-        } catch (error) {
-          setDisplayVal("Error");
-        }
-        break;
-      case "clear":
-        setDisplayVal("");
-        setMemory(0);
-        break;
-      case "memory":
-        switch (text) {
-          case "MS":
-            setDisplayVal("button MS");
-            break;
-          case "MC":
-            setDisplayVal("button MC");
-            break;
-          case "MR":
-            setDisplayVal("button MR");
-            break;
-          case "M+":
-            setDisplayVal("button M+");
-            break;
-          case "M-":
-            setDisplayVal("button M-");
-            break;
-          default:
-            break;
-        }
-        break;
-      case "percentage":
-        try {
-          const result = eval(displayVal);
-          setDisplayVal((result / 100).toString());
-        } catch (error) {
-          setDisplayVal("Error.");
-        }
-        break;
-      case "sign":
-        setDisplayVal((parseFloat(displayVal) * -1).toString());
-        break;
-      case "sqrt":
-        setDisplayVal(Math.sqrt(parseFloat(displayVal)).toString());
-        break;
       default:
         setDisplayVal("button works");
     }
