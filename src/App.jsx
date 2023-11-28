170c7e6dc07b69982188e7ee73df9e7067663ef3
Josh B <joshua.banaga@yahoo.com>
2023-11-28 12:17:12 -0800

    did the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,75 @@
-import { useState } from 'react';
-import '../public/styles/App.css';
-import Header from './Header';
-import Screen from './Screen';
-import Keypad from './Keypad';
+import { memo, useState } from "react";
+import "../public/styles/App.css";
+import Header from "./Header";
+import Screen from "./Screen";
+import Keypad from "./Keypad";
 function App() {
-  const headerText = 'React Calculator';
+  const headerText = "React Calculator";
 
   //state for the currently displayed key
-  const [displayVal, setDisplayVal] = useState('');
+  const [displayVal, setDisplayVal] = useState("");
+  const [memory, setMemory] = useState(0);
 
   //The callback function that will be passed to every button component.
   const onBtnClick = ({ type, text, value }) => {
     switch (type) {
+      case "number":
+        if (text === "." && displayVal.includes(".")) {
+          break;
+        }
+        setDisplayVal((prevDisplayVal) => prevDisplayVal + text);
+        break;
+      case "operator":
+        setDisplayVal((prevDisplayVal) => prevDisplayVal + text);
+        break;
+      case "equal":
+        try {
+          setDisplayVal(eval(displayVal).toString());
+        } catch (error) {
+          setDisplayVal("Error");
+        }
+        break;
+      case "clear":
+        setDisplayVal("");
+        setMemory(0);
+        break;
+      case "memory":
+        switch (text) {
+          case "MS":
+            setDisplayVal("button MS");
+            break;
+          case "MC":
+            setDisplayVal("button MC");
+            break;
+          case "MR":
+            setDisplayVal("button MR");
+            break;
+          case "M+":
+            setDisplayVal("button M+");
+            break;
+          case "M-":
+            setDisplayVal("button M-");
+            break;
+          default:
+            break;
+        }
+        break;
+      case "percentage":
+        try {
+          const result = eval(displayVal);
+          setDisplayVal((result / 100).toString());
+        } catch (error) {
+          setDisplayVal("Error.");
+        }
+        break;
+      case "sign":
+        setDisplayVal((parseFloat(displayVal) * -1).toString());
+        break;
+      case "sqrt":
+        setDisplayVal(Math.sqrt(parseFloat(displayVal)).toString());
+        break;
       default:
-        setDisplayVal('button works');
+        setDisplayVal("button works");
     }
   };
 
